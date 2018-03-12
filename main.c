ca45da481b603639763513930df05a15a2e543c8
Unknown <mario.romero.sandoval@outlook.com>
2018-03-11 22:11:53 -0600

    Arctan function

--- main.c
+++ main.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-#include <setjmp.h>
-#include <math.h>
-#include <unistd.h>
 
-
-long double function(int iter){
-    return pow(-1,iter) * pow(1,(2*iter+1))/(2*iter + 1);
-}
+#include "arctan_func.h"
 
 int main(int argc, char** argv){
-    int iter = 0;
+    int iter = 1;
     long double val = 0.0;
     while(1){
-        val +=  function(iter++);
-        printf("%Lf\n",4 * val);
-        sleep(1);
+        val +=  arctan_aproximation(1,iter++);
+        printf("%Lf\n", val);
     }
     return 0;
 }
